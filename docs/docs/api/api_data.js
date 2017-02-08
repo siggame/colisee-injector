b86f50e61b110d1040c15e8be46db4bae11b45c5
Unknown <russleyshaw@gmail.com>
2017-02-07 20:33:07 -0600

    Add api docs

---
+++ docs/docs/api/api_data.js
@@ -0,0 +1,42 @@
+define({ "api": [
+  {
+    "type": "",
+    "url": "GET",
+    "title": "/ Home",
+    "version": "0.0.0",
+    "filename": "src/main.js",
+    "group": "_home_russley_projects_siggame_colisee_injector_src_main_js",
+    "groupTitle": "_home_russley_projects_siggame_colisee_injector_src_main_js",
+    "name": "Get"
+  },
+  {
+    "type": "",
+    "url": "POST",
+    "title": "/api/inject Inject",
+    "version": "0.0.0",
+    "filename": "src/main.js",
+    "group": "_home_russley_projects_siggame_colisee_injector_src_main_js",
+    "groupTitle": "_home_russley_projects_siggame_colisee_injector_src_main_js",
+    "name": "Post"
+  },
+  {
+    "type": "",
+    "url": "GET",
+    "title": "/ Home",
+    "version": "0.0.0",
+    "filename": "src/main.ts",
+    "group": "_home_russley_projects_siggame_colisee_injector_src_main_ts",
+    "groupTitle": "_home_russley_projects_siggame_colisee_injector_src_main_ts",
+    "name": "Get"
+  },
+  {
+    "type": "",
+    "url": "POST",
+    "title": "/api/inject Inject",
+    "version": "0.0.0",
+    "filename": "src/main.ts",
+    "group": "_home_russley_projects_siggame_colisee_injector_src_main_ts",
+    "groupTitle": "_home_russley_projects_siggame_colisee_injector_src_main_ts",
+    "name": "Post"
+  }
+] });
