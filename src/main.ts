95c8c1862ec4e13ce64255b4c9a2e8d7632de33b
lonegalaxy <hannah@master-logic.com>
2017-02-11 00:08:59 -0600

    completed

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import * as path from "path";
 import * as express from "express";
 
-import { inject } from "./injector";
-
 let app = express()
 
 /**
@@ -40,28 +38,6 @@ const compB: number = req.query.competetorB; // id
   .catch((err)=>{
   	res.status(400).send();
   });
-});
-}
-
-app.post('/api/inject', (req, res) =>{
-
-    let p1 = req.query.player1;
-    let p2 = req.query.player2;
-
-    if(p1 == null || p2 == null) {
-        return res.status(400).send();
-    }
-    
-    // promise to inject
-    inject([p1, p2])
-        .then(()=>{
-            // is good
-            res.status(201).send();
-        })
-        .catch((err)=>{
-            // is bad
-            return res.status(400).send(err);
-        });
 });
 
 app.listen(3000, ()=>{
