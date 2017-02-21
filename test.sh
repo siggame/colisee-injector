886bad8d33f22bffc79281d24d04598e5c573c1e
Matthew Qualls <mqggd@mst.edu>
2017-02-21 17:43:29 -0600

    Changed docker to pull Stable DB

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker pull siggame/colisee-db
+docker pull siggame/colisee-db:stable
 docker rm --force injector-db
-docker run --name injector-db --publish 5432:5432 --detach siggame/colisee-db
+docker run --name injector-db --publish 5432:5432 --detach siggame/colisee-db:stable
 
 npm run test
 
