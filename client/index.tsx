74d8bf474e9e293c6693e115804944be65d28d8f
lonegalaxy <hannah@master-logic.com>
2017-02-28 18:05:45 -0600

    merged further, accidentally ran code in template

--- client/index.tsx
+++ client/index.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
+const element = <teamList name = 'Sara' />;
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  element,
   document.getElementById('root')
 );
