b747544b7b16a5a122ca55b4d5f893e74536ef29
Unknown <russleyshaw@gmail.com>
2017-02-28 22:17:18 -0600

    Do the import

--- client/index.tsx
+++ client/index.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-const element = <teamList name = 'Sara' />;
+import { TeamList } from "./TeamList";
+
 ReactDOM.render(
-  element,
+  <TeamList name='Sara' />,
   document.getElementById('root')
 );
